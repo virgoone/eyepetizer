29f2a50952f333acde6b3a2522942d6181a805a7
virgo <w2008second@gmail.com>
2017-02-21 17:47:39 +0800

    fixed index of project

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,20 +2,10 @@
   <div class="hello">
     <img src="../assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    
+    <h2>Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><router-link :to="'home'">开眼首页</router-link></li>
-      <li><router-link :to="'detail'">视频详情</router-link></li>
-      <li><router-link :to="'player'">视频播放</router-link></li>
+      <li><router-link :to="'detail/14416'">视频详情</router-link></li>
     </ul>
   </div>
 </template>
@@ -32,22 +22,25 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="scss" scoped>
+.hello{
+  text-align:center;
+  margin-top:60px;
+  &,& a{
+    color: #fff;
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #fff;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 }
 </style>
